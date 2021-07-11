c44285356b999f8ae922168a46d164fa1ddaaee9
sunbee <sanjay.bhatikar@gmail.com>
2021-07-10 23:21:54 -0500

    Added sub to pub client, millis not delay() for non-blocking code.

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,26 @@
 #include <PubSubClient.h>
 
 #include <SECRETS.h>
+#define TIMER_INTERVAL 12000
 #define onboard_led 16
 
 const char* SSID = SECRET_SSID;
 const char* PASS = SECRET_PASS;
 char broker_ip[] = SECRET_BROKER_IP;
+unsigned long tic = millis();
 
 WiFiClient WiFiClient;
-PubSubClient Broker(WiFiClient);
+PubSubClient MosquittoClient(WiFiClient);
+
+void mosquittoDo(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Got a message in topic ");
+  Serial.println(topic);
+  Serial.print("Received data: ");
+  for (int i = 0; i <= length; i++) {
+    Serial.print(char(payload[i]));
+  }
+  Serial.println();
+}
 
 void setup() {
   pinMode(onboard_led, OUTPUT);
@@ -30,13 +42,15 @@ void setup() {
   Serial.print("Connected: ");
   Serial.println(SSID);
 
-  Broker.setServer(broker_ip, 1883);
+  MosquittoClient.setServer(broker_ip, 1883);
+  MosquittoClient.setCallback(mosquittoDo);
 }
 
 void reconnect() {
-  while (!Broker.connected()) {
-    if (Broker.connect("NodeMCU", "fire_up_your_neurons", NULL)) {
+  while (!MosquittoClient.connected()) {
+    if (MosquittoClient.connect("NodeMCU", "fire_up_your_neurons", NULL)) {
       Serial.println("Uh-Kay!");
+      MosquittoClient.subscribe("Test"); // SUBSCRIBE TO TOPIC
     } else {
       Serial.print("Retrying ");
       Serial.println(broker_ip);
@@ -49,18 +63,24 @@ void publish_message() {
   String msg_payload = "Namaste from India";
   char char_buffer[999];
   msg_payload.toCharArray(char_buffer, 999);
-  Broker.publish("Test", char_buffer);
-  delay(1200);
+  MosquittoClient.publish("Test", char_buffer);
 }
 
 void loop() {
+  unsigned long toc = millis();
+
   // put your main code here, to run repeatedly:
   digitalWrite(onboard_led, LOW);
-  if (!Broker.connected()) {
-    Serial.println("Made no MQTT connection.");
-    reconnect();
-  } else {
-    digitalWrite(onboard_led, HIGH);
-    publish_message();
+  if (toc - tic > TIMER_INTERVAL) {
+    tic = toc;
+    if (!MosquittoClient.connected()) {
+      Serial.println("Made no MQTT connection.");
+      reconnect();
+    } else {
+      digitalWrite(onboard_led, HIGH);
+      publish_message();
+    }
   }
+  MosquittoClient.loop();
 }
+
