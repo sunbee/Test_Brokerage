789489ef52907c0921e01d8c1f73f9897bfe46b2
sunbee <sanjay.bhatikar@gmail.com>
2021-07-11 23:43:59 -0500

    Fixed issue with message format 4 display.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 
+
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int sensorPin = A0;   // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -18,17 +28,40 @@ unsigned long tic = millis();
 WiFiClient WiFiClient;
 PubSubClient MosquittoClient(WiFiClient);
 
+void displayMessage(String mess) {
+  /*
+  Use in the callback for MQTT subscriber.
+  The callback is 'mosquittoDo()'.
+  */
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,14);
+  display.println(mess);
+  display.display();
+  delay(2001);
+  display.clearDisplay();
+}
+
 void mosquittoDo(char* topic, byte* payload, unsigned int length) {
   Serial.print("Got a message in topic ");
   Serial.println(topic);
   Serial.print("Received data: ");
+  char message2display[length];
   for (unsigned int i = 0; i < length; i++) {
     Serial.print(char(payload[i]));
+    message2display[i] = payload[i];
   }
+  char message4OLED[length+6]; // 'Got: ' and null terminator.
+  snprintf(message4OLED, length+6, "Got: %s", message2display); 
   Serial.println();
+  displayMessage(message4OLED);
 }
 
 void setup() {
+  // initialize with the I2C addr 0x3C
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+  display.clearDisplay();
   pinMode(onboard_led, OUTPUT);
 
   // put your setup code here, to run once:
